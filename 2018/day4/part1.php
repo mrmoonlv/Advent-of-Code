46558f5e249ce6bf0fc9e7727f32dab65e83a757
moon <karlis.millers@gmail.com>
2021-12-02 22:50:49 +0200

    2021 day 4 part 1

--- 2018/day4/part1.php
+++ 2018/day4/part1.php
@@ -1,77 +1,82 @@
 <?php
 
-//Sarg
-
-$items = file("data.txt");
-
+$logs = file("data.txt");
+$sorted_logs = [];
 $data = [];
-$guard = null;
-$date = null;
-$min = null;
-$state = null;
+$active_guard = null;
+$guard_sleep = false;
+$sleep = null;
+$awake = null;
+$minutes_guard_sleeps = [];
+$date = '';
 
-
-function getDateInfo($item){
-    $parts = explode("]", $item); 
+foreach ($logs as $log) {
+    $parts = explode("]", $log);
     $string = str_replace("[", "", $parts[0]);
     $timestamp = strtotime($string);
-
-    return array(
-        'timestamp' => $timestamp,
-        'day' => date('m-d', $timestamp),
-        'min' => date('i', $timestamp)
-     );
+    $sorted_logs[$timestamp] = $log;
 }
+ksort($sorted_logs);
 
+foreach ($sorted_logs as  $timestamp => $log) {
+    $parts = explode(']' , $log);
+    $date = substr($parts[0], -11, 5);
 
-function sortItems($items){
+    if(strpos($log, 'begins shift') !== false) {
+        preg_match('!\d+!', $parts[1], $matches);
+        $current_guard = (int) $matches[0];
 
-    $sorted_items = [];
+        if($current_guard != $active_guard) {
+            $active_guard = $current_guard;
+//            echo "Guard changes...Date:: " . $date . PHP_EOL;
+            $awake = null;
+            $sleep = null;
+        }
+//        echo "Active guard: #" . $current_guard . PHP_EOL;
 
-    foreach ($items as $item) {
+        if(empty($minutes_guard_sleeps[$current_guard])) {
+            $minutes_guard_sleeps[$current_guard] = 0;
+        }
 
-        $date_info = getDateInfo($item);
-        $event = array(
-            'item' => $item,
-            'date' => $date_info
-            );
-
-        $sorted_items[$date_info['timestamp']] = $event;
     }
 
-    ksort($sorted_items);
-
-    return $sorted_items;
-}
-
-function checkGuard($item, $guard){
-    if (strpos($item, '#') === false) {
-        return $guard;
+    if(strpos($log, 'falls asleep') !== false) {
+        $sleep = (int) substr($parts[0], -2);
+//        echo "Sleep: " . $sleep . PHP_EOL;
     }
 
-    $parts = explode("#", $item);
-    $guard = floatval($parts[1]);
+    if(strpos($log, 'wakes up') !== false) {
+        $awake = (int) substr($parts[0], -2);
+//        echo "Wakes up: " . $awake . PHP_EOL;
 
-    return $guard;
-}
+        $total_sleep = $awake - $sleep;
+//        echo sprintf("Guard #%s sleep from %s to %s . Total %s minutes", $active_guard, $sleep, $awake, $total_sleep) . PHP_EOL;
+        $minutes_guard_sleeps[$current_guard] = $minutes_guard_sleeps[$current_guard] + $total_sleep;
 
-function checkGuardState($item){
-    if (strpos($item, 'asleep') !== false) {
-        return 'sleep';
+        for($sleep; $sleep < $awake; $sleep++){
+            $data[$active_guard][$sleep][] = 1;
+        }
+
+        $sleep = null;
+        $awake = null;
+        $total_sleep = null;
     }
-    return 'awake';
 }
 
-$i = 1;
-$sorted_items = sortItems($items);
+$most_sleepy_guard = array_search(max($minutes_guard_sleeps), $minutes_guard_sleeps);
+$max_times_in_popular_minute = 0;
+$popular_minute = 0;
 
-print_r($sorted_items);
-
-foreach ($sorted_items as $item) {
-    $guard = checkGuard($item, $guard);
-    $state = checkGuardState($item);
-    $date = getDateInfo($item);
-    $data[$guard][$date['day']][$state][] = $date['min'];
+foreach ($data[$most_sleepy_guard] as $minute => $times){
+    if($max_times_in_popular_minute < count($times)) {
+        $max_times_in_popular_minute = count($times);
+        $popular_minute = $minute;
+    }
 }
 
-print_r($data);
+$id = $most_sleepy_guard * $popular_minute;
+
+echo sprintf("The ID of the guard multiplied by the minute is: %s", $id) . PHP_EOL;
+if(8421 == $id) {
+    echo "Answer is correct!";
+}
